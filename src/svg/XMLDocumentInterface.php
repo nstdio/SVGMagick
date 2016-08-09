03edaa8bd2c8dfa053dca3a71ab68c34c9776a6c
Edgar Asatryan <nstdio@gmail.com>
2016-08-09 18:41:44 +0400

    Adding phpDoc.

--- src/svg/XMLDocumentInterface.php
+++ src/svg/XMLDocumentInterface.php
@@ -7,35 +7,65 @@ interface XMLDocumentInterface
     public function getElement();
 
     /**
-     * @param $value
+     * @param mixed $value The text value of node.
      */
     public function setNodeValue($value);
 
     /**
-     * @param array $except
+     * @param array $except Returned array will not contain attributes specified in this array.
      *
      * @return array Associative array where the keys are the name of the attribute node.
      */
     public function attributes(array $except);
 
+    /**
+     * @param string $name
+     * @param mixed $value
+     * @return void
+     */
     public function setAttribute($name, $value = null);
 
+    /**
+     * @param string $name Attribute name.
+     * @return string Attribute value.
+     */
     public function getAttribute($name);
 
+    /**
+     * @param $namespaceURI
+     * @param $qualifiedName
+     * @param $value
+     * @return string
+     */
     public function setAttributeNS($namespaceURI, $qualifiedName, $value);
 
+    /**
+     * @param $namespaceURI
+     * @param $localName
+     * @return mixed
+     */
     public function getAttributeNS($namespaceURI, $localName);
 
+    /**
+     * @param XMLDocumentInterface $newNode
+     * @return void
+     */
     public function appendChild(XMLDocumentInterface $newNode);
 
     /**
-     * @param      $name
-     * @param null $value
+     * @param string $name
+     * @param mixed $value
      *
      * @return XMLDocumentInterface A new element.
      */
     public function createElement($name, $value = null);
 
+    /**
+     * @param string $namespaceURI
+     * @param string $qualifiedName
+     * @param string $value
+     * @return mixed
+     */
     public function createElementNS($namespaceURI, $qualifiedName, $value = null);
 
     /**
