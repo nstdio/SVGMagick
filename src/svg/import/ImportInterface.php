656884473057627a3255d3b13bb5f5938d14100f
Edgar Asatryan <nstdio@gmail.com>
2016-08-28 22:24:16 +0400

    Adding interface to import svg string and converting it into object.

---
+++ src/svg/import/ImportInterface.php
@@ -0,0 +1,13 @@
+<?php
+namespace nstdio\svg\import;
+
+/**
+ * Interface ImportInterface
+ *
+ * @package nstdio\svg\import
+ * @author  Edgar Asatryan <nstdio@gmail.com>
+ */
+interface ImportInterface
+{
+
+}
