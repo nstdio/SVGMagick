cce9233fd525897415e8a0a38bcc0fdf82618c7e
nstdio <nstdio@gmail.com>
2017-05-11 21:31:56 +0400

    Try to fix travis build on PHP 7.

--- tests/svg/SVGContextTestCase.php
+++ tests/svg/SVGContextTestCase.php
@@ -2,6 +2,10 @@
 use nstdio\svg\Base;
 use nstdio\svg\container\SVG;
 
+if (!class_exists('\PHPUnit_Framework_TestCase') && class_exists('\PHPUnit\Framework\TestCase')) {
+    class_alias('\PHPUnit\Framework\TestCase', '\PHPUnit_Framework_TestCase');
+}
+
 /**
  * Class TestCase
  *
